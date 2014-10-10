880844695d31bef4b0a2836eda67f0c100b81d2e
Anton Koldaev <me@iroller.ru>
2014-10-10 01:09:47 -0700

    Update community endpoint

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@
 #
 
 berkshelf_api node['berkshelf-api']['path'] do
-  opscode_endpoint
+  supermarket_endpoint
   auto_chef_server_endpoint unless Chef::Config[:solo]
   proxy if node['berkshelf-api']['proxy']['enabled']
 end
